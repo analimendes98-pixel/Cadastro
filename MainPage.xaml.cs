ffef2ca128c8598238b835a389fcd825c5917150
analimendes98-pixel <anali.mendes98@gmail.com>
2025-11-16 21:53:53 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Cadastro.Models;
+using Cadastro.Views;
+
+namespace Cadastro
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Evento _evento = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = _evento;
+        }
+
+        private async void onConcluirCadastroClicked(object? sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ResumoEventoPage(_evento));
+        }
+    }
+}
+
+
+
+
+
+
